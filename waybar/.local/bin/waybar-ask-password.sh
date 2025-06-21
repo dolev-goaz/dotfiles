4e2df28eea6e3b53273af0908c041bb421585618
dolev-goaz <www.dolev@gmail.com>
2025-06-22 02:46:40 +0300

    simple update

--- waybar/.local/bin/waybar-ask-password.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-exec yad --entry \
-	--title="Authentication Required" \
-	--text="Enter your sudo password:" \
-	--hide-text
--- waybar/.local/bin/waybar-update-system.sh
+++ waybar/.local/bin/waybar-update-system.sh
@@ -4,20 +4,6 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-export SUDO_ASKPASS="$HOME/.local/bin/waybar-ask-password.sh"
-
-if ! sudo -A -k true 2>/dev/null; then
-	notify-send "Update cancelled" "Wrong password or cancelled by user."
-	exit 1
-fi
-
-log=$(mktemp)
-
-~/scripts/update-arch.sh &>"$log" &
-update_pid=$!
-notify-send "Updating system" "Update PID: $update_pid\nUpdate Log: $log" --icon=dialog-information
-
-wezterm start -- zsh -c "tail -f $log"
-wait "$update_pid"
-notify-send "Update complete" "Your system has been updated successfully."
-rm "$log"
+notify-send "Update started" "System update started in new terminal." --icon=dialog-information
+wezterm start -- zsh -c "~/scripts/update-arch.sh; echo 'Press any key to continue'; read -n 1 -s -r"
+notify-send "Update finished" "System update finished." --icon=dialog-information
