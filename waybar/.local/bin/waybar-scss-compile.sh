4134ba1fa01e292cad8fac730ee7d7409c3ea5f3
dolev-goaz <www.dolev@gmail.com>
2025-06-21 17:06:09 +0300

    placeholder variable name

--- waybar/.local/bin/waybar-scss-compile.sh
+++ waybar/.local/bin/waybar-scss-compile.sh
@@ -9,7 +9,7 @@ cp "$SCSS" "$TMP"
 sed -i -E 's/@import/__IMPORT_PLACEHOLDER__/g; s/@keyframes/__KEYFRAMES_PLACEHOLDER__/g' "$TMP"
 
 # 3. Replace @var with --var
-sed -i -E 's/@([a-zA-Z0-9_-]+)/--\1/g' "$TMP"
+sed -i -E 's/@([a-zA-Z0-9_-]+)/--PLACEHOLDER_VARIABLE_\1/g' "$TMP"
 
 # 4. Restore placeholders
 sed -i -E 's/__IMPORT_PLACEHOLDER__/@import/g; s/__KEYFRAMES_PLACEHOLDER__/@keyframes/g' "$TMP"
@@ -17,7 +17,7 @@ sed -i -E 's/__IMPORT_PLACEHOLDER__/@import/g; s/__KEYFRAMES_PLACEHOLDER__/@keyf
 sass "$TMP" "$OUT"
 
 # 5. Replace -- with @ in the final CSS to make it GTK-compatible again
-sed -i -E 's/\--([a-zA-Z0-9_-]+)/@\1/g' "$OUT"
+sed -i -E 's/\--PLACEHOLDER_VARIABLE_([a-zA-Z0-9_-]+)/@\1/g' "$OUT"
 
 # 6. Clean up
 rm "$TMP" "$OUT.map"
