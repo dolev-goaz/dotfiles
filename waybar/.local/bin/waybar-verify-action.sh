03a3decd16a341b6816679a7ae272c1b179595ab
dolev-goaz <www.dolev@gmail.com>
2025-06-22 00:46:37 +0300

    optional command

--- waybar/.local/bin/waybar-verify-action.sh
+++ waybar/.local/bin/waybar-verify-action.sh
@@ -2,9 +2,15 @@
 # theme with nwg-look as yad's gtk
 ACTION_NAME="$1"
 COMMAND="$2"
-
-yad \
+if yad \
 	--text="\n<span font='16'>$ACTION_NAME?</span>\n\n\n" \
 	--text-align="center" \
-	--button='Cancel':0 --button="$ACTION_NAME":1 &&
-	"$COMMAND"
+	--button='Cancel':1 --button="$ACTION_NAME":0; then
+	if [ -n "$COMMAND" ]; then
+		"$COMMAND"
+	else
+		exit 0
+	fi
+else
+	exit 1
+fi
