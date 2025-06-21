80d034007ef071536bdeafdcf1a5e4af546743dd
dolev-goaz <www.dolev@gmail.com>
2025-06-21 20:22:45 +0300

    another sass syntax supported

--- waybar/.local/bin/waybar-scss-compile.sh
+++ waybar/.local/bin/waybar-scss-compile.sh
@@ -5,14 +5,14 @@ OUT=~/.config/waybar/style.css
 # 1. Copy to a temporary working file
 cp "$SCSS" "$TMP"
 
-# 2. Extract @import and @keyframes temporarily
-sed -i -E 's/@import/__IMPORT_PLACEHOLDER__/g; s/@keyframes/__KEYFRAMES_PLACEHOLDER__/g' "$TMP"
+# 2. Extract @import, @keyframes and @extend temporarily
+sed -i -E 's/@import/__IMPORT_PLACEHOLDER__/g; s/@keyframes/__KEYFRAMES_PLACEHOLDER__/g; s/@extend/__EXTEND_PLACEHOLDER__/g' "$TMP"
 
 # 3. Replace @var with --var
 sed -i -E 's/@([a-zA-Z0-9_-]+)/--PLACEHOLDER_VARIABLE_\1/g' "$TMP"
 
 # 4. Restore placeholders
-sed -i -E 's/__IMPORT_PLACEHOLDER__/@import/g; s/__KEYFRAMES_PLACEHOLDER__/@keyframes/g' "$TMP"
+sed -i -E 's/__IMPORT_PLACEHOLDER__/@import/g; s/__KEYFRAMES_PLACEHOLDER__/@keyframes/g; s/__EXTEND_PLACEHOLDER__/@extend/g' "$TMP"
 
 sass "$TMP" "$OUT"
 
