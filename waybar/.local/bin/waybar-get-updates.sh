13e46d20277ed148bebd50e0bab6460e08b8573f
dolev-goaz <www.dolev@gmail.com>
2025-07-07 00:19:45 +0300

    fixed syntax

--- waybar/.local/bin/waybar-get-updates.sh
+++ waybar/.local/bin/waybar-get-updates.sh
@@ -8,8 +8,8 @@ official=$(echo "$updates" | jq .official)
 aur=$(echo "$updates" | jq .aur)
 total=$(echo "$updates" | jq .total)
 
-if [ "$total" -eq 0 ] then
-    exit 0
+if [ "$total" -eq 0 ]; then
+	exit 0
 fi
 
 css_class="ok"
