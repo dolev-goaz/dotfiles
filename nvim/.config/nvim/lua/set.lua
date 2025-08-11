f458c17a75906b6ea35e54261f1197cf078ee124
dolev-goaz <www.dolev@gmail.com>
2025-08-11 19:42:32 +0300

    undo search remapping

--- nvim/.config/nvim/lua/set.lua
+++ nvim/.config/nvim/lua/set.lua
@@ -107,7 +107,7 @@ vim.api.nvim_set_keymap(
 
 local langmap_normal = [[אt,בc,גd,דs,הv,וu,זz,חj,טy,יh,כf,לk,מn,נb,סx,עg,פp,צm,קe,רr,שa,ת>]]
 local langmap_final = [[ךl,םo,ןi]] .. "," .. [[ף\\;]] .. "," .. [[ץ.]]
-local langmap_special = [[/q,\\'w]] .. "," .. "][,[]" .. "," .. "./" .. "," .. [[\\,\\']]
+local langmap_special = [[\\'w]] .. "," .. "][,[]" .. "," .. "./" .. "," .. [[\\,\\']] -- .. "," .. [[/q]]
 local langmap = langmap_normal .. "," .. langmap_final .. "," .. langmap_special
 
 vim.cmd("set langmap=" .. langmap)
