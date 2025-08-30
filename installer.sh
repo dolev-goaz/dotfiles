3cc65b8fecbcb8cc17b28d44b2f3f3764d792ad0
dolev-goaz <www.dolev@gmail.com>
2025-08-30 20:19:36 +0300

    installer for dotfiles

---
+++ installer.sh
@@ -0,0 +1,27 @@
+chmod +x ./generate-stow-local-ignore.sh
+
+sudo pacman -S --needed \
+	stow
+
+clear
+
+packages=()
+
+for package_dir in */; do
+	package_name="${package_dir%/}"
+	packages+=("$package_name")
+done
+
+selected=$(
+	printf \
+		"%s\n" "${packages[@]}" |
+		fzf --multi --reverse --prompt="Select packages(space to select, ctrl-a to select all): " \
+			--bind "space:toggle" --bind "ctrl-space:toggle" \
+			--bind "ctrl-a:toggle-all"
+)
+
+for package in $selected; do
+	chmod +x "$package/install.sh"
+	echo "==> Running install script for $package"
+	(cd "$package" && ./install.sh)
+done
