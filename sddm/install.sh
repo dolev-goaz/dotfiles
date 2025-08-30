3a311ebb84a97c5567bf3462a7d39107c4d4661a
dolev-goaz <www.dolev@gmail.com>
2025-08-30 20:36:37 +0300

    custom installer for sddm as it uses the global filesystem

--- sddm/install.sh
+++ sddm/install.sh
@@ -1,2 +1,22 @@
+sudo pacman -S --needed \
+	qt6-svg \
+	qt6-declarative \
+	qt5-quickcontrols2
+
+chmod o+x /home/dolev
+chmod o+x /home/dolev/.dotfiles
+chmod o+x /home/dolev/.dotfiles/sddm
+chmod o+r /home/dolev/.dotfiles/sddm/etc/sddm.conf
+chmod o+r /home/dolev/.dotfiles/sddm/etc/X11
+chmod o+r /home/dolev/.dotfiles/sddm/etc/X11/xorg.conf.d
+chmod o+r /home/dolev/.dotfiles/sddm/etc/X11/xorg.conf.d/20-touchpad.conf
+
+chmod o+x /home/dolev/.dotfiles/sddm/usr
+chmod o+x /home/dolev/.dotfiles/sddm/usr/share
+chmod o+x /home/dolev/.dotfiles/sddm/usr/share/sddm
+chmod o+x /home/dolev/.dotfiles/sddm/usr/share/sddm/themes
+chmod o+x /home/dolev/.dotfiles/sddm/usr/share/sddm/themes/catppuccin-mocha
+chmod -R o+r /home/dolev/.dotfiles/sddm/usr/share/sddm/themes/catppuccin-mocha
+
 cd ~/.dotfiles
-stow sddm
+sudo stow -t / sddm
