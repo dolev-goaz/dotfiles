a93cee0d5a96898671955e642ebc1d13ca2f5985
dolev-goaz <www.dolev@gmail.com>
2025-08-31 02:03:57 +0300

    setup copilot chat

--- nvim/.config/nvim/lua/plugins/copilot-chat.lua
+++ nvim/.config/nvim/lua/plugins/copilot-chat.lua
@@ -4,13 +4,10 @@ local function toggle_copilot_chat()
 	if is_copilot_open then
 		copilot_chat.close()
 	else
-		copilot_chat.open({
-			context = { "buffer" },
-		})
+		copilot_chat.open()
 	end
 	is_copilot_open = not is_copilot_open
 end
-
 return {
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
@@ -20,9 +17,24 @@ return {
 			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
 		},
 		build = "make tiktoken",
-		config = function()
-			require("CopilotChat").setup({})
-			vim.keymap.set("n", "<C-c>", toggle_copilot_chat, { desc = "Toggle Copilot Chat" })
+		---@type CopilotChat.config.Config
+		opts = {
+			-- sticky = {
+			-- 	"$gpt-4.1",
+			-- 	"#filenames:*",
+			-- },
+			resources = {
+				"buffers:visible",
+				"selection",
+				-- "diagnostics:current",
+			},
+			tools = {
+				"copilot",
+			},
+		},
+		config = function(_, opts)
+			require("CopilotChat").setup(opts)
+			vim.keymap.set({ "n", "v", "i" }, "<C-c>", toggle_copilot_chat, { desc = "Toggle Copilot Chat" })
 		end,
 	},
 }
