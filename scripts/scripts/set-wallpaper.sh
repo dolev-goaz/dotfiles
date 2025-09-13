258d5823308762dca55c931dadd4e7ac22a26734
dolev-goaz <www.dolev@gmail.com>
2025-09-13 22:25:08 +0300

    set background image in sddm

--- scripts/scripts/set-wallpaper.sh
+++ scripts/scripts/set-wallpaper.sh
@@ -4,4 +4,6 @@ swww img "$1" --transition-type grow --transition-duration 1.0 --transition-fps 
 # create a blurred version of the wallpaper for hyprlock
 magick "$1" -blur 0x8 ~/Pictures/Wallpapers/active-blurred.png
 
-# TODO: handle sddm
+# store the blurred wallpaper in the sddm theme folder
+cp ~/Pictures/Wallpapers/active-blurred.png /usr/share/sddm/themes/catppuccin-mocha/backgrounds/active-blurred.png
+sync # ensure the file is written before locking
