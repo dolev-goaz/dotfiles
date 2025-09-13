ada6314c27d8ae5b183df0a0675fd77284afabc2
dolev-goaz <www.dolev@gmail.com>
2025-09-13 22:45:29 +0300

    if the mocha theme was installed

--- scripts/scripts/set-wallpaper.sh
+++ scripts/scripts/set-wallpaper.sh
@@ -4,6 +4,7 @@ swww img "$1" --transition-type grow --transition-duration 1.0 --transition-fps 
 # create a blurred version of the wallpaper for hyprlock
 magick "$1" -blur 0x8 ~/Pictures/Wallpapers/active-blurred.png
 
-# store the blurred wallpaper in the sddm theme folder
-cp ~/Pictures/Wallpapers/active-blurred.png /usr/share/sddm/themes/catppuccin-mocha/backgrounds/active-blurred.png
-sync # ensure the file is written before locking
+if [ -d /usr/share/sddm/themes/catppuccin-mocha/backgrounds ]; then
+	cp ~/Pictures/Wallpapers/active-blurred.png /usr/share/sddm/themes/catppuccin-mocha/backgrounds/active-blurred.png
+	sync # ensure the file is written before locking
+fi
