5d15d40d8b6c82eb1d7a8b35ac4d30b1b61a09e2
dolev-goaz <www.dolev@gmail.com>
2025-09-17 12:52:31 +0300

    udiskie docs

--- udiskie/install.sh
+++ udiskie/install.sh
@@ -6,3 +6,14 @@ sudo pacman -S --needed \
 
 cd ~/.dotfiles
 stow udiskie
+
+# NODE: need to install polkit. sudo pacman -S polkit
+# NOTE: need /etc/polkit-1/rules.d/50-udisks2-mount.rules to have-
+# polkit.addRule(function(action, subject) {
+#     if (
+#         (action.id.indexOf("org.freedesktop.udisks2.") == 0) &&
+#         subject.isInGroup("wheel")
+#     ) {
+#         return polkit.Result.YES;
+#     }
+# });
