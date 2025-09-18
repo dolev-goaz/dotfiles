d16fbb24fa10b4277b052047fcc30ac32fd73ad6
dolev-goaz <www.dolev@gmail.com>
2025-09-18 21:49:01 +0300

    swaync installed separately

--- hyprland/install.sh
+++ hyprland/install.sh
@@ -2,7 +2,6 @@ sudo pacman -S --needed \
 	hyprland \
 	hyprshot \
 	hyprpicker \
-	swaync \
 	xdg-desktop-portal-hyprland
 
 # xdg-desktop-portal-hyprland is needed for screen sharing in browsers and other apps
