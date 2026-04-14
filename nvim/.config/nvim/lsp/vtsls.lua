c59dfc9550aa99ede1f79a4acf698599e38847e1
dolev-goaz <www.dolev@gmail.com>
2026-04-14 17:57:22 +0300

    can't return the value because of merge ordering

--- nvim/.config/nvim/lsp/vtsls.lua
+++ nvim/.config/nvim/lsp/vtsls.lua
@@ -1,26 +1,31 @@
 -- typescript+vue lsp
 -- reference: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#vue-support
 -- https://github.com/vuejs/language-tools/wiki/Neovim
+
+-- NOTE: We call vim.lsp.config() here instead of returning a table.
+-- Return values from lsp/*.lua go into rtp_config and are merged first.
+-- vim.lsp.config() calls go into _configs[name] which is merged last (wins).
+-- This ensures 'vue' is not overwritten by nvim-lspconfig's lsp/vtsls.lua,
+-- which only includes the 4 JS/TS filetypes.
 local vue_language_server_path = vim.fn.stdpath("data")
 	.. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
-local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
-local vue_plugin = {
-	name = "@vue/typescript-plugin",
-	location = vue_language_server_path,
-	languages = { "vue" },
-	configNamespace = "typescript",
-}
 
----@type vim.lsp.Config
-return {
+vim.lsp.config("vtsls", {
+	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
 	settings = {
 		vtsls = {
 			tsserver = {
 				globalPlugins = {
-					vue_plugin,
+					{
+						name = "@vue/typescript-plugin",
+						location = vue_language_server_path,
+						languages = { "vue" },
+						configNamespace = "typescript",
+					},
 				},
 			},
 		},
 	},
-	filetypes = tsserver_filetypes,
-}
+})
+
+return {}
