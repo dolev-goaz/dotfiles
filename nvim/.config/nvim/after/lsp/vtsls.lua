6a63a77b437d37562e89f274863ac989d34eff07
dolev-goaz <www.dolev@gmail.com>
2026-04-14 17:59:44 +0300

    load vtsls last

--- nvim/.config/nvim/after/lsp/vtsls.lua
+++ nvim/.config/nvim/after/lsp/vtsls.lua
@@ -2,15 +2,13 @@
 -- reference: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#vue-support
 -- https://github.com/vuejs/language-tools/wiki/Neovim
 
--- NOTE: We call vim.lsp.config() here instead of returning a table.
--- Return values from lsp/*.lua go into rtp_config and are merged first.
--- vim.lsp.config() calls go into _configs[name] which is merged last (wins).
--- This ensures 'vue' is not overwritten by nvim-lspconfig's lsp/vtsls.lua,
--- which only includes the 4 JS/TS filetypes.
+-- placed in after/lsp/ so it loads last in the rtp merge, overriding
+-- nvim-lspconfig's lsp/vtsls.lua which omits 'vue' from filetypes.
 local vue_language_server_path = vim.fn.stdpath("data")
 	.. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
 
-vim.lsp.config("vtsls", {
+---@type vim.lsp.Config
+return {
 	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
 	settings = {
 		vtsls = {
@@ -26,6 +24,4 @@ vim.lsp.config("vtsls", {
 			},
 		},
 	},
-})
-
-return {}
+}
