84361cbcd91eae8bb100837903791f5df4af1e4a
dolev-goaz <www.dolev@gmail.com>
2026-04-15 18:24:27 +0300

    wrong binding

--- nvim/.config/nvim/lua/plugins/session.lua
+++ nvim/.config/nvim/lua/plugins/session.lua
@@ -5,7 +5,7 @@ vim.defer_fn(function()
 	require("auto-session").setup({
 		auto_create = false,
 	})
-	vim.keymap.set("n", "<leader>ss", ":AutoSession search<CR>", { desc = "[s]ession [s]earch" })
+	vim.keymap.set("n", "<leader>sf", ":AutoSession search<CR>", { desc = "[s]ession [f]ind" })
 	vim.keymap.set("n", "<leader>sd", ":AutoSession delete ", { desc = "[s]ession [d]elete" })
 	vim.keymap.set("n", "<leader>sr", ":AutoSession restore<CR>", { desc = "[s]ession [r]estore" })
 	vim.keymap.set("n", "<leader>sc", ":AutoSession save ", { desc = "[s]ession [c]reate" })
