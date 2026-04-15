1bf509d80b3e0aac043550f613e797a412f0c7ef
dolev-goaz <www.dolev@gmail.com>
2026-04-16 00:30:15 +0300

    continue vim.pack

--- nvim/.config/nvim/lua/plugins/markdown-view.lua
+++ nvim/.config/nvim/lua/plugins/markdown-view.lua
@@ -1,4 +1,3 @@
-return {
-	"OXY2DEV/markview.nvim",
-	lazy = false,
-}
+vim.pack.add({
+	"https://github.com/OXY2DEV/markview.nvim",
+})
--- nvim/.config/nvim/lua/plugins/notify.lua
+++ nvim/.config/nvim/lua/plugins/notify.lua
@@ -1,114 +1,106 @@
-return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		{
-			"rcarriga/nvim-notify",
-			config = function()
-				require("notify").setup({
-					merge_duplicates = true,
-					max_width = 60,
-					stages = "fade_in_slide_out",
-					render = "wrapped-default",
-					background_colour = "#000000",
-					timeout = 3000,
-					on_open = function(win)
-						vim.api.nvim_win_set_config(win, {
-							focusable = false,
-						})
-					end,
-				})
-				vim.notify = require("notify")
-			end,
-		},
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("noice").setup({
-			cmdline = {
-				format = {
-					calculator = false,
-				},
-			},
-			views = {
-				popup = {
-					scrollbar = false,
-				},
-			},
-			lsp = {
-				hover = {
-					enabled = false,
-				},
-			},
-			messages = { enabled = true },
-			routes = {
-				-- Git status
-				{
-					filter = {
-						event = "msg_show",
-						any = {
-							{ find = "Your branch" },
-							{ find = "On branch" },
-							{ find = "^Enumerating objects:" },
-							{ find = "^Counting objects:" },
-							{ find = "^Compressing objects:" },
-							{ find = "^Writing objects: " },
-							{ find = "^remote: " },
-						},
-					},
-					view = "popup",
-				},
-				{
-					filter = {
-						event = "msg_show",
-						any = {
-							{ find = "^Saved working directory" },
-						},
-					},
-					view = "notify",
-				},
-				{
-					filter = {
-						event = "msg_show",
-						any = {
-							{ find = "^fatal:" },
-							{ find = "error: failed to push some refs" },
-						},
-					},
-					-- --- @type NoiceNotifyOptions -- remove first two -- for autocomplete
-					opts = {
-						level = vim.log.levels.ERROR,
-						title = "Git Error",
-					},
-					view = "notify",
-				},
-				{
-					filter = {
-						any = {
-							{ find = "Toggling hidden files" },
-							{ find = "^Restored session:" },
-							{ find = "%[LSP%]" }, -- escape square brackets
-						},
-					},
-					view = "mini",
-				},
-				-- {
-				-- 	filter = {
-				-- 		min_height = 8,
-				-- 	},
-				-- 	view = "popup",
-				-- },
-			},
+vim.pack.add({
+	-- dependencies
+	"https://github.com/MunifTanjim/nui.nvim",
+	-- plugins
+	"https://github.com/rcarriga/nvim-notify",
+	"https://github.com/folke/noice.nvim",
+})
+
+require("notify").setup({
+	merge_duplicates = true,
+	max_width = 60,
+	stages = "fade_in_slide_out",
+	render = "wrapped-default",
+	background_colour = "#000000",
+	timeout = 3000,
+	on_open = function(win)
+		vim.api.nvim_win_set_config(win, {
+			focusable = false,
 		})
-		-- Custom print message
-		_G.print = function(...)
-			local args = { ... }
-			local msg = {}
-			for i = 1, #args do
-				msg[#msg + 1] = vim.inspect(args[i])
-			end
-			local message = table.concat(msg, " ")
-			vim.notify(message, vim.log.levels.INFO, { title = "Print" })
-		end
 	end,
-}
+})
+vim.notify = require("notify")
+
+require("noice").setup({
+	cmdline = {
+		format = {
+			calculator = false,
+		},
+	},
+	views = {
+		popup = {
+			scrollbar = false,
+		},
+	},
+	lsp = {
+		hover = {
+			enabled = false,
+		},
+	},
+	messages = { enabled = true },
+	routes = {
+		-- Git status
+		{
+			filter = {
+				event = "msg_show",
+				any = {
+					{ find = "Your branch" },
+					{ find = "On branch" },
+					{ find = "^Enumerating objects:" },
+					{ find = "^Counting objects:" },
+					{ find = "^Compressing objects:" },
+					{ find = "^Writing objects: " },
+					{ find = "^remote: " },
+				},
+			},
+			view = "popup",
+		},
+		{
+			filter = {
+				event = "msg_show",
+				any = {
+					{ find = "^Saved working directory" },
+				},
+			},
+			view = "notify",
+		},
+		{
+			filter = {
+				event = "msg_show",
+				any = {
+					{ find = "^fatal:" },
+					{ find = "error: failed to push some refs" },
+				},
+			},
+			-- --- @type NoiceNotifyOptions -- remove first two -- for autocomplete
+			opts = {
+				level = vim.log.levels.ERROR,
+				title = "Git Error",
+			},
+			view = "notify",
+		},
+		{
+			filter = {
+				any = {
+					{ find = "Toggling hidden files" },
+					{ find = "^Restored session:" },
+					{ find = "%[LSP%]" }, -- escape square brackets
+				},
+			},
+			view = "mini",
+		},
+	},
+})
+
+-- Custom print message
+_G.print = function(...)
+	local args = { ... }
+	local msg = {}
+	for i = 1, #args do
+		msg[#msg + 1] = vim.inspect(args[i])
+	end
+	local message = table.concat(msg, " ")
+	vim.notify(message, vim.log.levels.INFO, { title = "Print" })
+end
+
+return {}
