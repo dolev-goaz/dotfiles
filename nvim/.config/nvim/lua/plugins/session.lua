2da645cb2e87b172bba489b19b22b5276f5a3b18
dolev-goaz <www.dolev@gmail.com>
2026-04-16 18:18:22 +0300

    dont load sessions lazily

--- nvim/.config/nvim/lua/plugins/session.lua
+++ nvim/.config/nvim/lua/plugins/session.lua
@@ -1,15 +1,31 @@
 return {
 	"rmagatti/auto-session",
-	event = "VeryLazy",
+	lazy = false,
 	keys = {
 		{ "<leader>sf", ":AutoSession search<CR>", desc = "[s]ession [f]ind" },
 		{ "<leader>sd", ":AutoSession delete ", desc = "[s]ession [d]elete" },
 		{ "<leader>sr", ":AutoSession restore<CR>", desc = "[s]ession [r]estore" },
 		{ "<leader>sc", ":AutoSession save ", desc = "[s]ession [c]reate" },
 	},
+	init = function()
+		vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+	end,
 	---@module "auto-session"
 	---@type AutoSession.Config
 	opts = {
 		auto_create = false,
+		disabled_filetypes = { "alpha", "neo-tree" },
+		suppressed_dirs = { "~/", "~/Downloads", "/" },
+		session_lens = {
+			session_control = {
+				pre_save_cmds = {
+					function()
+						for _, client in ipairs(vim.lsp.get_clients()) do
+							client:stop()
+						end
+					end,
+				},
+			},
+		},
 	},
 }
