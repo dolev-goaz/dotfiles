1c6d564cce33fad2074f3777faf94469be77c250
dolev-goaz <www.dolev@gmail.com>
2026-04-16 12:33:38 +0300

    migrated lualine to vim.pack

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -16,191 +16,193 @@ local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
 	end
 end
 
-return {
-	"nvim-lualine/lualine.nvim",
-	event = "VeryLazy",
-	enabled = vim.o.laststatus ~= 0,
-	dependencies = {
+if vim.o.laststatus == 0 then
+	return {}
+end
+vim.pack.add({
+	"https://github.com/cameronr/lualine-pretty-path",
+	"https://github.com/nvim-lualine/lualine.nvim",
+})
+local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+
+local encoding_only_if_not_utf8 = function()
+	local ret, _ = (vim.bo.fenc or vim.go.enc):gsub("^utf%-8$", "")
+	return ret
+end
+local fileformat_only_if_not_unix = function()
+	local ret, _ = vim.bo.fileformat:gsub("^unix$", "")
+	return ret
+end
+
+local lualine_winbar = {
+	lualine_a = {
 		{
-			"cameronr/lualine-pretty-path",
-			-- dev = true,
+			"pretty_path",
+			directories = {
+				max_depth = 4,
+			},
+			highlights = {
+				newfile = "LazyProgressDone",
+			},
+			separator = "",
 		},
 	},
-	opts = function()
-		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
-
-		local encoding_only_if_not_utf8 = function()
-			local ret, _ = (vim.bo.fenc or vim.go.enc):gsub("^utf%-8$", "")
-			return ret
-		end
-		local fileformat_only_if_not_unix = function()
-			local ret, _ = vim.bo.fileformat:gsub("^unix$", "")
-			return ret
-		end
-
-		return {
-			options = {
-				theme = require("lualine.themes." .. vim.g.colors_name),
-				section_separators = { left = "", right = "" },
-
-				disabled_filetypes = { "alpha", "neo-tree" },
-				globalstatus = true,
+	lualine_b = {
+		{
+			"navic",
+			color_correction = nil,
+			navic_opts = {
+				depth_limit = 4,
 			},
-			winbar = {
-				lualine_a = {
-					{
-						"pretty_path",
-						directories = {
-							max_depth = 4,
-						},
-						highlights = {
-							newfile = "LazyProgressDone",
-						},
-						separator = "",
-					},
-				},
-				lualine_b = {
-					{
-						"navic",
-						color_correction = nil,
-						navic_opts = {
-							depth_limit = 4,
-						},
-					},
-				},
-			},
-			sections = {
-				lualine_a = {
-					{
-						"mode",
-						fmt = trunc(130, 3, 0, true),
-						separator = { left = " ", right = "" },
-					},
-				},
-				lualine_b = {
-					{
-						"branch",
-						fmt = trunc(70, 15, 65, true),
-					},
-				},
-				lualine_c = {},
-				lualine_x = {
-					{
-						"diagnostics",
-						symbols = { error = " ", warn = " ", info = " ", hint = " " },
-						update_in_insert = true,
-					},
-					{
-						"diff",
-						symbols = {
-							added = " ",
-							modified = " ",
-							removed = " ",
-						},
-						fmt = trunc(0, 0, 60, true),
-					},
-					{
-						function()
-							return "recording @" .. vim.fn.reg_recording()
-						end,
-						cond = function()
-							return vim.fn.reg_recording() ~= ""
-						end,
-						color = { fg = "#ff007c" },
-					},
-					{
-
-						function()
-							local res = vim.fn.searchcount()
-							if not res then
-								return ""
-							end
-							local search_string = vim.fn.getreg("/")
-							local base_format = search_string -- string.format('searching "%s"', search_string)
-							if res["incomplete"] == 0 then
-								return string.format("%s [%d/%d]", base_format, res["current"], res["total"])
-							end
-							if res["incomplete"] == 1 then
-								return string.format(" [?/??]", base_format)
-							end
-							-- incomplete = 2, timeout
-							-- if both current and total exceed maxcount
-							if res["total"] > res["maxcount"] and res["current"] > res["maxcount"] then
-								return string.format("%s [>%d/>%d]", base_format, res["current"], res["total"])
-							else
-								-- if only total exceeds maxcount
-								if res["total"] > res["maxcount"] then
-									return string.format("%s [%d/>%d]", base_format, res["current"], res["total"])
-								end
-							end
-						end,
-						icon = "",
-						color = function()
-							local res = vim.fn.searchcount()
-							local is_empty = res["total"] == 0
-							return { fg = is_empty and "#d08770" or "#ffffff" }
-						end,
-					},
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						fmt = trunc(0, 0, 160, true),
-						separator = "",
-					},
-					{
-						encoding_only_if_not_utf8,
-						fmt = trunc(0, 0, 140, true),
-					},
-					{
-						fileformat_only_if_not_unix,
-						fmt = trunc(0, 0, 140, true),
-					},
-				},
-				lualine_y = {
-					{
-						function()
-							return require("auto-session.lib").current_session_name(true)
-						end,
-						icon = "",
-					},
-				},
-				lualine_z = {
-					{
-						"location",
-						fmt = trunc(0, 0, 80, true),
-						icon = "",
-						separator = { left = "", right = " " },
-					},
-				},
-			},
-			inactive_sections = {},
-			inactive_winbar = {
-				lualine_a = {
-					{
-						"pretty_path",
-						directories = {
-							max_depth = 4,
-						},
-						highlights = {
-							newfile = "LazyProgressDone",
-						},
-						separator = { left = "", right = "" },
-					},
-				},
-				lualine_b = {
-					{
-						"navic",
-						color_correction = nil,
-						navic_opts = nil,
-					},
-				},
-			},
-			extensions = {
-				"lazy",
-				"mason",
-				"neo-tree",
-				"quickfix",
-			},
-		}
-	end,
+		},
+	},
 }
+local lualine_inactive_winbar = {
+	lualine_a = {
+		{
+			"pretty_path",
+			directories = {
+				max_depth = 4,
+			},
+			highlights = {
+				newfile = "LazyProgressDone",
+			},
+			separator = { left = "", right = "" },
+		},
+	},
+	lualine_b = {
+		{
+			"navic",
+			color_correction = nil,
+			navic_opts = nil,
+		},
+	},
+}
+
+local lualine_sections = {
+	lualine_a = {
+		{
+			"mode",
+			fmt = trunc(130, 3, 0, true),
+			separator = { left = " ", right = "" },
+		},
+	},
+	lualine_b = {
+		{
+			"branch",
+			fmt = trunc(70, 15, 65, true),
+		},
+	},
+	lualine_c = {},
+	lualine_x = {
+		{
+			"diagnostics",
+			symbols = { error = " ", warn = " ", info = " ", hint = " " },
+			update_in_insert = true,
+		},
+		{
+			"diff",
+			symbols = {
+				added = " ",
+				modified = " ",
+				removed = " ",
+			},
+			fmt = trunc(0, 0, 60, true),
+		},
+		{
+			function()
+				return "recording @" .. vim.fn.reg_recording()
+			end,
+			cond = function()
+				return vim.fn.reg_recording() ~= ""
+			end,
+			color = { fg = "#ff007c" },
+		},
+		{
+
+			function()
+				local res = vim.fn.searchcount()
+				if not res then
+					return ""
+				end
+				local search_string = vim.fn.getreg("/")
+				local base_format = search_string -- string.format('searching "%s"', search_string)
+				if res["incomplete"] == 0 then
+					return string.format("%s [%d/%d]", base_format, res["current"], res["total"])
+				end
+				if res["incomplete"] == 1 then
+					return string.format(" [?/??]", base_format)
+				end
+				-- incomplete = 2, timeout
+				-- if both current and total exceed maxcount
+				if res["total"] > res["maxcount"] and res["current"] > res["maxcount"] then
+					return string.format("%s [>%d/>%d]", base_format, res["current"], res["total"])
+				else
+					-- if only total exceeds maxcount
+					if res["total"] > res["maxcount"] then
+						return string.format("%s [%d/>%d]", base_format, res["current"], res["total"])
+					end
+				end
+			end,
+			icon = "",
+			color = function()
+				local res = vim.fn.searchcount()
+				local is_empty = res["total"] == 0
+				return { fg = is_empty and "#d08770" or "#ffffff" }
+			end,
+		},
+		{
+			lazy_status.updates,
+			cond = lazy_status.has_updates,
+			fmt = trunc(0, 0, 160, true),
+			separator = "",
+		},
+		{
+			encoding_only_if_not_utf8,
+			fmt = trunc(0, 0, 140, true),
+		},
+		{
+			fileformat_only_if_not_unix,
+			fmt = trunc(0, 0, 140, true),
+		},
+	},
+	lualine_y = {
+		{
+			function()
+				return require("auto-session.lib").current_session_name(true)
+			end,
+			icon = "",
+		},
+	},
+	lualine_z = {
+		{
+			"location",
+			fmt = trunc(0, 0, 80, true),
+			icon = "",
+			separator = { left = "", right = " " },
+		},
+	},
+}
+
+local lualine_inactive_sections = {}
+local lualine_extensions = { "lazy", "mason", "neo-tree", "quickfix" }
+
+vim.defer_fn(function()
+	require("lualine").setup({
+		options = {
+			theme = require("lualine.themes." .. vim.g.colors_name),
+			section_separators = { left = "", right = "" },
+
+			disabled_filetypes = { "alpha", "neo-tree" },
+			globalstatus = true,
+		},
+		winbar = lualine_winbar,
+		inactive_winbar = lualine_inactive_winbar,
+		sections = lualine_sections,
+		inactive_sections = lualine_inactive_sections,
+		extensions = lualine_extensions,
+	})
+	-- delay to let other plugins load
+end, 500)
+
+return {}
