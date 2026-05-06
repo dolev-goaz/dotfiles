d0b5a455741e35303c0ca93be0d63ee90b07b0dd
dolev-goaz <www.dolev@gmail.com>
2026-05-06 14:18:54 +0300

    lsp highlights in nvim

--- nvim/.config/nvim/lua/lsp-hover-highlight.lua
+++ nvim/.config/nvim/lua/lsp-hover-highlight.lua
@@ -1,3 +1,12 @@
+local function is_lsp_supported()
+	local clients = vim.lsp.get_clients({ bufnr = 0 })
+	for _, client in ipairs(clients) do
+		if client.server_capabilities.documentHighlightProvider then
+			return true
+		end
+	end
+	return false
+end
 -- highlight hovered word and its references
 -- NOTE: could use treesitter to not 'flicker' on the same word
 local hover_augroup = vim.api.nvim_create_augroup("LSP_reference_highlight", { clear = true })
@@ -5,7 +14,7 @@ vim.api.nvim_create_autocmd("CursorHold", {
 	group = hover_augroup,
 	desc = "Highlight hovered word and its references",
 	callback = function()
-		if vim.api.nvim_get_mode().mode ~= "n" then
+		if vim.api.nvim_get_mode().mode ~= "n" or not is_lsp_supported() then
 			return
 		end
 		vim.lsp.buf.document_highlight()
