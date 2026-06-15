331c15a2091c805129c1888c5589410aed12b7cd
dolev-goaz <www.dolev@gmail.com>
2026-06-15 13:46:56 +0300

    weylus launch is more versatile

--- weylus/.local/bin/weylus-launch.sh
+++ weylus/.local/bin/weylus-launch.sh
@@ -1,5 +1,20 @@
-hyprctl output create headless Virtual-1
+if pgrep -x "weylus" >/dev/null; then
+	killall weylus
+	fuser -k 1701/tcp
+fi
+
+# Virtual monitor for weylus
+VIRTUAL_MONITOR_NAME="Virtual-1"
+MONITOR_EXISTS=$(hyprctl monitors -j | jq --arg name "$VIRTUAL_MONITOR_NAME" 'any(.[]; .name == $name)')
+if [ "$MONITOR_EXISTS" = "false" ]; then
+	hyprctl output create headless "$VIRTUAL_MONITOR_NAME"
+fi
+
+adb reverse tcp:1701 tcp:1701
+
+# can pass --no-gui to run in the background
 weylus \
+	--no-gui \
 	--bind-address "0.0.0.0" \
 	--web-port 1701 \
 	--gui-theme dark \
@@ -7,4 +22,3 @@ weylus \
 	--auto-start \
 	--try-nvenc \
 	--access-code="I AM DOLEV"
-adb reverse tcp:1701 tcp:1701
